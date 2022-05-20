233d2da3c5ef1879434312bcc856b8e41bef0b8f
hgf777-br <hgf777@gmail.com>
2022-05-20 10:54:21 -0300

    allowed hosts

--- boat/settings.py
+++ boat/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m+e8k5=&)$*0%34lgdzd%x1)pa=x0#!*bx82*4@kp7_xj1d=-f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
